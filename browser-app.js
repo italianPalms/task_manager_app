e131b3b8cc505260b365cf162d555823e1cf0850
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-07 16:06:20 +0200

    add JS file for browser app, and add eventListener for dark mode

---
+++ browser-app.js
@@ -0,0 +1,8 @@
+
+const switchContainer = document.getElementById('switchContainer');
+const toggleSwitch = document.getElementById('toggleSwitch');
+
+switchContainer.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    toggleSwitch.checked = !toggleSwitch.checked;
+});
