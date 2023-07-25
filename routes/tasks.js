e45944ef00d24a4406b49d3655f13dcad6b596ea
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-25 12:16:07 +0200

    add variables for all api requests (create, update, delete, get)

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,10 +3,15 @@
 const express = require('express')
 const router = express.Router();
 
-const {getAllTasks} = require('../controllers/tasks')
+const {
+    getAllTasks, 
+    createTask, 
+    getTask, 
+    updateTask,
+    deleteTask
+} = require('../controllers/tasks')
 
-router.route('/').get((req, res) => {
-    res.send('all items')
-})
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router
