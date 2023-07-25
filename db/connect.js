d7b2ee427035a89703e54413e022f205311a90b1
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-25 13:58:33 +0200

    add a variable to connect to db, and refactor code

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose')
 
 const connectionString = "mongodb+srv://Eirik:TaskManager123@task-manager-app.2gz7czb.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose
+const connectDB = (url) => {
+    return mongoose
     .connect(connectionString)
-    .then(() => console.log('Connected to the DB...'))
-    .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
