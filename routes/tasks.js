cde1d72a1dc27f91c0321bc05082cdbdc91c2935
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-25 12:43:16 +0200

    add a comment for that descibes all routes, and add json to create and get single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -15,3 +15,10 @@ router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 module.exports = router
+
+// overview of routes
+// GET         - api/tasks         - get all tasks
+// POST        - api/tasks         - create tasks
+// GET         - api/tasks/:id     - get tasks
+// PUT/PATCH   - api/tasks/:id     - update tasks
+// DELETE      - api/tasks/:id     - delete taks 
