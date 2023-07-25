5c05cdbcb08675dac5b2720d02af17f4e9d1439d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-25 13:45:04 +0200

    connect to app.js to the db connect.js file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('./db/connect')
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
 
 //middleware
+
 app.use(express.json())
 
 // routes
@@ -15,5 +17,5 @@ app.use('/api/v1/tasks', tasks)
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is listening on port ${port}...`);
 });
