bff78172de8b23a6bf3f663fa5b34b027c0077d0
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-26 21:08:00 +0200

    add consts and query selectors to be able to show tasks from the the API

--- Public/browser-app.js
+++ Public/browser-app.js
@@ -1,7 +1,32 @@
 
+const tasksDOM = document.querySelector('.tasks');
+const loadingDOM = document.querySelector('.loading-text');
+const formDOM = document.querySelector('.task-form');
+const taskInputDOM = document.querySelector('.task-input');
+const formAlertDOM = document.querySelector('.form-alert');
 
-
-
+//load tasks from API
+const showTasks = async () => {
+    loadingDOM.computedStyleMap.visibility = 'visible'
+    try {
+        const {
+            data: {tasks}, 
+        } = await axios.get('/api/v1/tasks')
+        if (tasks.length < 1) {
+            tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
+            loadingDOM.style.visibility = 'hidden'
+            return
+        }
+        const allTasks = tasks
+            .map((task) => {
+                const { completed, _id: taskID, name } = task
+                return `<div class="single-task ${completed && 'task-completed'}">`
+                //continue here!!!
+            })
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+}
 
 
 
