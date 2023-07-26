b58a3ced3568f9a15d435c4015c597dbe76e968f
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-26 12:40:34 +0200

    add dotenv dependency and move mongo db details into .env file for security

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 
-const connectionString = "mongodb+srv://Eirik:TaskManager123@task-manager-app.2gz7czb.mongodb.net/?retryWrites=true&w=majority"
-
 const connectDB = (url) => {
     return mongoose
-    .connect(connectionString)
+    .connect(url)
 }
 
 module.exports = connectDB
