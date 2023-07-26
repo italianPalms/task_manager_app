b7263074db2300d382ce0622a710f1e7190f499d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-26 19:02:24 +0200

    add API request for delete task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -18,16 +18,34 @@ const createTask = async (req, res) => {
     }
 }
 
-const getTask = (req, res) => {
-    res.json({id:req.params.id})
+const getTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOne({_id: taskID})
+        if (!task) {
+            return res.status(404).json({msg: `No task with id : ${taskID}` })
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 const updateTask = (req, res) => {
     res.send('update task')
 }
 
-const deleteTask = (req, res) => {
-    res.send('delete task')
+const deleteTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndDelete({_id: taskID})
+        if (!task) {
+            return res.status(404).json({msg: `No task with id: ${taskID}` })
+        }
+        res.status(200).json({task, status: 'success'})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
 }
 
 module.exports = {
