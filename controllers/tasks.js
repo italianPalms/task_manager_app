8473c73386505ea2bf6b311d1bd0423a0e952750
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-26 19:25:59 +0200

    add functionality to update task by id

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,10 +31,6 @@ const getTask = async (req, res) => {
     }
 }
 
-const updateTask = (req, res) => {
-    res.send('update task')
-}
-
 const deleteTask = async (req, res) => {
     try {
         const {id: taskID} = req.params
@@ -43,6 +39,24 @@ const deleteTask = async (req, res) => {
             return res.status(404).json({msg: `No task with id: ${taskID}` })
         }
         res.status(200).json({task, status: 'success'})
+    } catch (error) {
+        res.status(500).json({msg: error})
+    }
+}
+
+const updateTask = async (req, res) => {
+    try {
+        const {id: taskID} = req.params
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body, {
+            new: true, 
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).json({msg: `No task with id: ${taskID}` })
+        }
+
+        res.status(200).json({task, status: 'success'})
+
     } catch (error) {
         res.status(500).json({msg: error})
     }
