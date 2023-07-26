1f31f1e671f61cf152d1043133020303086e5eee
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-26 13:42:33 +0200

    fix permission issues in mongodb

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,8 +5,8 @@ const getAllTasks = (req, res) => {
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json( {task} )
+    const task = await Task.create(req.body )
+    res.status(201).json({ task })
 }
 
 const getTask = (req, res) => {
