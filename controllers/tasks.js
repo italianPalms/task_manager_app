113a2c20d9ad764c224714ccdcb457d681698f8d
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-28 13:40:28 +0200

    remove empty lines

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -54,9 +54,7 @@ const updateTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({msg: `No task with id: ${taskID}` })
         }
-
         res.status(200).json({task, status: 'success'})
-
     } catch (error) {
         res.status(500).json({msg: error})
     }
