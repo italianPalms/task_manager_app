0940e5bfd070191f8407ccd598be17ec47263520
Eirik Nordsveen <eiriknordsveen@gmail.com>
2023-07-28 21:21:35 +0200

    try to make axios to work

--- Public/browser-app.js
+++ Public/browser-app.js
@@ -10,7 +10,7 @@ const showTasks = async () => {
     loadingDOM.computedStyleMap.visibility = 'visible'
     try {
         const {
-            data: {tasks}, 
+            data: { tasks },
         } = await axios.get('/api/v1/tasks')
         if (tasks.length < 1) {
             tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
